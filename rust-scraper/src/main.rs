515100aeca0a6fb2d263b074150d7b56d7a68a78
sdornel <kai_dornel@hotmail.com>
2025-03-28 21:10:32 -0400

    fake user agent

--- rust-scraper/src/main.rs
+++ rust-scraper/src/main.rs
@@ -1,8 +1,11 @@
 
 use std::fs; // file parsing
 use std::error::Error; // allows us to use Result for error handling
+use ua_generator::ua::spoof_ua; // fake user agent
+use reqwest::header::USER_AGENT; // allows to set user agent
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
     let json_data = fs::read_to_string("../output/urls.json")?;
     let urls: Vec<String> = serde_json::from_str(&json_data)?;
@@ -11,12 +14,32 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     for (i, url) in urls.iter().enumerate() {
         println!("{} => {}", i, url);
-        // match fetch_url(url).await {}
+        match fetch_url(url).await {
+            Ok(body) => println!("✅ Success: {} bytes\n", body.len()),
+            Err(error) => println!("❌ Failed to fetch: {}\n", error),
+        }
     }
 
     Ok(())
 }
 
-// async fn fetch_url(url: &str) -> Result<String, Box<dyn Error>> {
+async fn fetch_url(url: &str) -> Result<String, Box<dyn Error>> {
+    let user_agent = spoof_ua();
 
-// }
+    let client = reqwest::Client::new();
+    
+    // let response = reqwest::get(url).await?;
+    let response = client
+        .get(url)
+        .header(USER_AGENT, user_agent)
+        .send()
+        .await?;
+
+    let status = response.status(); 
+    if !status.is_success() {
+        return Err(format!("Http error: {}", status).into());
+    }
+
+    let body = response.text().await?;
+    Ok(body)
+}
