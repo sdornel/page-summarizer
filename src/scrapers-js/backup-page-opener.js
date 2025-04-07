5aa8108eddfbb4a41357fe20fbbe6ecf14b1d9a1
sdornel <kai_dornel@hotmail.com>
2025-04-07 00:18:56 -0400

    improve stealth and performance

--- src/scrapers-js/backup-page-opener.js
+++ src/scrapers-js/backup-page-opener.js
@@ -7,12 +7,65 @@ const correlationId = process.argv[3];
 const replySubject = process.argv[4];
 
 const browser = await puppeteer.launch({
-  headless: true,
-  args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: true,
+    // protocolTimeout: 20000, // ⬅️ Timeout for internal DevTools Protocol calls
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-blink-features=AutomationControlled",
+      "--disable-web-security",
+      "--disable-features=IsolateOrigins,site-per-process",
+      "--no-zygote",
+      "--single-process",
+      "--disable-dev-shm-usage",
+      "--window-size=1366,768",
+    ],
 });
 const page = await browser.newPage();
 
-await page.setUserAgent(faker.internet.userAgent());
+const languageOptions = [
+    ["en-US", "en"],
+    ["en-GB", "en"],
+    ["fr-FR", "fr"],
+    ["de-DE", "de"],
+    ["es-ES", "es"],
+    ["it-IT", "it"],
+    ["pl-PL", "pl"],
+    ["ru-RU", "ru"],
+    ["ja-JP", "ja"],
+    ["zh-CN", "zh"],
+    ["pt-BR", "pt"],
+    ["nl-NL", "nl"],
+    ["sv-SE", "sv"],
+    ["tr-TR", "tr"],
+    ["ko-KR", "ko"]
+];
+
+const languages = faker.helpers.arrayElement(languageOptions);
+  
+    await page.setUserAgent(faker.internet.userAgent());
+    await page.evaluateOnNewDocument((langs) => {
+        Object.defineProperty(navigator, "languages", { get: () => langs });
+    }, lang);
+  
+await page.setRequestInterception(true);
+
+// Block images, stylesheets, fonts, and media to speed up loading
+page.on("request", (req) => {
+    const type = req.resourceType();
+    if (["image", "stylesheet", "font", "media"].includes(type)) req.abort();
+    else req.continue();
+});
+
+
+// Spoof navigator props
+await page.evaluateOnNewDocument((langs) => {
+    Object.defineProperty(navigator, "webdriver", { get: () => false });
+    Object.defineProperty(navigator, "platform", { get: () => "Win32" });
+    Object.defineProperty(navigator, "language", { get: () => langs[0] });
+    Object.defineProperty(navigator, "languages", { get: () => langs });
+    Object.defineProperty(navigator, "plugins", { get: () => [1, 2, 3, 4, 5] });
+}, languages);
 
 await page.goto(url, { waitUntil: "domcontentloaded", timeout: 10000 });
 const text = await page.evaluate(() => document.body.innerText);
